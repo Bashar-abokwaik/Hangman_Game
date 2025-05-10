638840a1b946d765fa84e0ebb8cd7131c6ecdc32
Bashar-abokwaik <basharabokaik@gmail.com>
2025-05-10 23:42:00 +0300

    update

--- JS/main.js
+++ JS/main.js
@@ -9,32 +9,63 @@ let lettersContainer = document.querySelector(".letters");
 
 //Generat Letters
 lettersArray.forEach((letter) => {
-    //Create Span
-    let span = document.createElement("span");
+  //Create Span
+  let span = document.createElement("span");
 
-    //Create Letter Text Node
-    let theLatter = document.createTextNode(letter);
+  //Create Letter Text Node
+  let theLatter = document.createTextNode(letter);
 
-    //Append the Letter To Span
-    span.appendChild(theLatter);
+  //Append the Letter To Span
+  span.appendChild(theLatter);
 
-    //Add Class On Span 
-    span.className = "letter-box";
+  //Add Class On Span
+  span.className = "letter-box";
 
-    //Append Span To The Letters Container
-    lettersContainer.appendChild(span);
-
+  //Append Span To The Letters Container
+  lettersContainer.appendChild(span);
 });
 
 //Object Of Words + Categories
 const words = {
-    Programming: ["php", "javascript", "go", "scala", "fortran", "r", "mysql", "python"],
-    movies: ["Prestige", "Inception", "Parasite", "Interstallar", "Whiplash", "Memento", "Coco", "Up"],
-    people: ["Albert Einstein", "Hitchcock", "Alexander", "cleopatra", "Mahatma Ghandi"],
-    countries: ["Syria", "Palestine", "Jordan", "Yemen", "Egypt", "Bahrein", "Qatar"],
-}
+  Programming: [
+    "php",
+    "javascript",
+    "go",
+    "scala",
+    "fortran",
+    "r",
+    "mysql",
+    "python",
+  ],
+  movies: [
+    "Prestige",
+    "Inception",
+    "Parasite",
+    "Interstallar",
+    "Whiplash",
+    "Memento",
+    "Coco",
+    "Up",
+  ],
+  people: [
+    "Albert Einstein",
+    "Hitchcock",
+    "Alexander",
+    "cleopatra",
+    "Mahatma Ghandi",
+  ],
+  countries: [
+    "Syria",
+    "Palestine",
+    "Jordan",
+    "Yemen",
+    "Egypt",
+    "Bahrein",
+    "Qatar",
+  ],
+};
 
-//Get Random Property 
+//Get Random Property
 
 let allKeys = Object.keys(words);
 
@@ -54,6 +85,28 @@ let randomValueNumber = Math.floor(Math.random() * randomPropValue.length);
 let randomValueName = randomPropValue[randomValueNumber];
 
 //Set Category Info
-document.querySelector(".game-info .category span").innerHTML = randomPropName + " " +randomValueName;
+document.querySelector(".game-info .category span").innerHTML = randomPropName;
 
+//Select Letters Guess Element
+let lettersGuessContainer = document.querySelector(".letters-guess");
 
+//Convert Chosen Word To Array
+let lettersAndSpace = Array.from(randomValueName);
+
+//Create Spans Depened On Word
+
+lettersAndSpace.forEach((letter) => {
+  //Create Empty Span
+  let emptySpan = document.createElement("span");
+
+  //If Letter is Space
+  if (letter === " ") {
+    // Add Class To The Span
+    emptySpan.className = "With-Space";
+  }
+
+  //Append Span To The Letters Guess Cotainer
+  lettersGuessContainer.appendChild(emptySpan);
+});
+
+console.log(randomValueName);
